ff4199e3d2e27280299885087290cc15f116d59a
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-07-18 10:17:51 +0800

    Initial Commit

---
+++ ZJJAudioPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJAudioPlay
+//
+//  Created by 张锦江 on 2017/7/18.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJJAudioPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJJAudioPlay
+//
+//  Created by 张锦江 on 2017/7/18.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
